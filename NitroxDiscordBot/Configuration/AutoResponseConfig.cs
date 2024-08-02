c7b23d7972bd088328b20c7ab5a6ab72f59cf820
Measurity <measuring.infinity@gmail.com>
2024-08-02 07:49:15 +0200

    Refactored AutoResponseConfig.cs to use enum instead of string values

--- NitroxDiscordBot/Configuration/AutoResponseConfig.cs
+++ NitroxDiscordBot/Configuration/AutoResponseConfig.cs
@@ -8,28 +8,37 @@ public class AutoResponseConfig
 
     public record Definition
     {
-        [Required]
-        public string Name { get; init; }
+        [Required] public string Name { get; init; }
+
         public Filter[] Filters { get; init; } = [];
         public Response[] Responses { get; init; } = [];
     }
 
     public record Filter
     {
-        [Required]
-        [RegularExpression("Channel|UserJoinTimeSpan|MessageWordOrder")]
-        public string Type { get; init; }
+        public enum Types
+        {
+            Channel,
+            UserJoinTimeSpan,
+            MessageWordOrder
+        }
+
+        [Required] public Types Type { get; init; }
+
         public object Value { get; init; }
-        [ConfigurationKeyName(nameof(Value))]
-        public object[] Values { get; init; }
+
+        [ConfigurationKeyName(nameof(Value))] public object[] Values { get; init; }
     }
 
     public record Response
     {
-        [Required]
-        [RegularExpression("MessageRoles")]
-        public string Type { get; init; }
-        [Required]
-        public string Value { get; init; }
+        public enum Types
+        {
+            MessageRoles
+        }
+
+        [Required] public Types Type { get; init; }
+
+        [Required] public string Value { get; init; }
     }
 }
--- NitroxDiscordBot/Services/AutoResponseService.cs
+++ NitroxDiscordBot/Services/AutoResponseService.cs
@@ -3,6 +3,7 @@ using Discord.WebSocket;
 using Microsoft.Extensions.Options;
 using NitroxDiscordBot.Configuration;
 using NitroxDiscordBot.Core;
+using static NitroxDiscordBot.Configuration.AutoResponseConfig;
 
 namespace NitroxDiscordBot.Services;
 
@@ -18,7 +19,7 @@ public class AutoResponseService : DiscordBotHostedService
         this.options = options ?? throw new ArgumentNullException(nameof(options));
     }
 
-    public IEnumerable<AutoResponseConfig.Definition> Definitions => options.CurrentValue.AutoResponseDefinitions;
+    public IEnumerable<Definition> Definitions => options.CurrentValue.AutoResponseDefinitions;
 
     public override async Task StartAsync(CancellationToken cancellationToken)
     {
@@ -41,15 +42,15 @@ public class AutoResponseService : DiscordBotHostedService
 
     private async Task ModerateMessageAsync(SocketGuildUser author, SocketUserMessage message)
     {
-        foreach (AutoResponseConfig.Definition definition in Definitions)
+        foreach (Definition definition in Definitions)
         {
             if (!MatchesFilters(definition.Filters, author, message)) continue;
 
-            foreach (AutoResponseConfig.Response response in definition.Responses)
+            foreach (Response response in definition.Responses)
             {
                 switch (response.Type)
                 {
-                    case "MessageRoles":
+                    case Response.Types.MessageRoles:
                         ulong[] roles = response.Value
                             .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
                             .Select(r => ulong.TryParse(r, out ulong roleId) ? roleId : 0).Where(r => r != 0)
@@ -68,20 +69,20 @@ public class AutoResponseService : DiscordBotHostedService
         }
     }
 
-    private bool MatchesFilters(AutoResponseConfig.Filter[] filters, SocketGuildUser author, SocketUserMessage message)
+    private bool MatchesFilters(Filter[] filters, SocketGuildUser author, SocketUserMessage message)
     {
-        foreach (AutoResponseConfig.Filter filter in filters)
+        foreach (Filter filter in filters)
         {
             switch (filter.Type)
             {
-                case "Channel" when filter.Value is string valueStr && ulong.TryParse(valueStr, out ulong channelId):
+                case Filter.Types.Channel when filter.Value is string valueStr && ulong.TryParse(valueStr, out ulong channelId):
                     if (message.Channel.Id != channelId) return false;
                     break;
-                case "UserJoinTimeSpan" when filter.Value is string valueStr &&
-                                             TimeSpan.TryParse(valueStr, out TimeSpan valueTimeSpan):
+                case Filter.Types.UserJoinTimeSpan when filter.Value is string valueStr &&
+                                                                              TimeSpan.TryParse(valueStr, out TimeSpan valueTimeSpan):
                     if (DateTimeOffset.UtcNow - author.JoinedAt > valueTimeSpan) return false;
                     break;
-                case "MessageWordOrder" when filter.Values is [..] values:
+                case Filter.Types.MessageWordOrder when filter.Values is [..] values:
                     string[] sentences = message.Content.Split(sentenceSplitCharacters,
                         StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
                     foreach (string sentence in sentences)
